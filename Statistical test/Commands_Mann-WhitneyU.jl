8163d3fe37141a26518e8d4b52167a3e48ecfce0
MiaBrunetti <69636270+MiaBrunetti@users.noreply.github.com>
2026-09-14 17:16:41 -0400

    Assign CSV read result to df_WT variable

--- Statistical test/Commands_Mann-WhitneyU.jl
+++ Statistical test/Commands_Mann-WhitneyU.jl
@@ -1,7 +1,7 @@
 using StatsBase, HypothesisTests, CSV, DataFrames
 
 # H3-WT
-CSV.read(joinpath(@__DIR__,"..", "Results", "Accepted Parameters (WT).csv"), DataFrame);
+df_WT = CSV.read(joinpath(@__DIR__,"..", "Results", "Accepted Parameters (WT).csv"), DataFrame);
 p_WT = Matrix(df_WT[:,3:end]);
 labs_WT = labs_WT;
 
